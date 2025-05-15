c0c7878a035625e5be795aaffedb20506cfc0916
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-05-16 00:27:58 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,9 @@ const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ 
+    origin: [process.env.FRONTEND_URL,'https://your-frontend.netlify.app' ],
+    credentials: true }));
 app.use(express.json());
 app.use(
   session({
